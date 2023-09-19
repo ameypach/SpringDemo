95af65c17de43240539b5781374e3b0b4240e118
ameypach <ameypach@gmail.com>
2023-09-19 14:47:48 +0530

    First commit

---
+++ DemoAQE/src/main/java/com/amdocs/controller/restcont.java
@@ -0,0 +1,14 @@
+package com.amdocs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class restcont {
+	
+	@GetMapping("/get")
+	public String show() {
+		return "Welcome to Spring boot And jenkin Demo";
+	}
+	
+
+}
